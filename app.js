f2c8ca7d1cd84337286a427bcc602e59a167c940
Tomiwa Obanla <obanlatomiwa@gmail.com>
2020-06-19 19:05:12 +0100

    new functionalities added

--- app.js
+++ app.js
@@ -36,4 +36,43 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
     let dice_display = document.querySelector('.dice');
     dice_display.style.display = 'block';
     dice_display.src = 'dice-' + dice + '.png';
-})
+
+    // change the player to the next person
+    if (dice !== 1){
+        roundScore += dice;
+        document.getElementById('current-' + activePlayer).textContent = roundScore;
+    }else{
+        nextPlayer();
+    }});
+
+// implement the hold functionality
+document.querySelector('.btn-hold').addEventListener('click', function(){
+    // update the scores
+    score[activePlayer] += roundScore;
+    document.getElementById('score-' + activePlayer).textContent = score[activePlayer];
+
+    // check for the winner
+    if (score[activePlayer] >= 20){
+        document.getElementById('name-' + activePlayer).textContent = 'Winner !!!';
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+    }else{
+    // change the player to the next person
+    nextPlayer();
+
+    }    
+
+});
+
+function nextPlayer(){
+    // change players
+    activePlayer === 1 ? activePlayer = 0 : activePlayer =  1;
+    roundScore = 0;
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-0').textContent = '0';
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+    document.querySelector('.dice').style.display = 'none';
+
+}
